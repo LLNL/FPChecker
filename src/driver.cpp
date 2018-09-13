8507c644185d6d44995418423bfb99151ceed5f9
Ignacio Laguna <ilaguna@llnl.gov>
2018-09-13 14:32:22 -0700

    new version to know if func is a kernel

--- src/driver.cpp
+++ src/driver.cpp
@@ -17,6 +17,9 @@
 #include "llvm/CodeGen/RegAllocRegistry.h"
 //#include "llvm/PassRegistry.h"
 #include "llvm/IR/LegacyPassManager.h"
+//#include "llvm/lib/Target/NVPTX/NVPTXUtilities.h"
+#include "llvm/Support/MutexGuard.h"
+#include "llvm/Support/ManagedStatic.h"
 
 #include <string>
 #include <iostream>
@@ -29,6 +32,14 @@ using namespace llvm;
 namespace CUDAAnalysis
 {
 
+namespace {
+typedef std::map<std::string, std::vector<unsigned> > key_val_pair_t;
+typedef std::map<const GlobalValue *, key_val_pair_t> global_val_annot_t;
+typedef std::map<const Module *, global_val_annot_t> per_module_annot_t;
+} // anonymous namespace
+static ManagedStatic<per_module_annot_t> annotationCache;
+static sys::Mutex Lock;
+
 class CUDAKernelAnalysis : public ModulePass
 {
 public:
@@ -36,7 +47,7 @@ public:
 
   CUDAKernelAnalysis() : ModulePass(ID) {}
 
-  bool isFunctionAKernel(Function *f)
+  /*bool isFunctionAKernel(Function *f)
   {
   	Module *m = f->getParent();
   	NamedMDNode *NMD = m->getNamedMetadata("nvvm.annotations");
@@ -72,6 +83,89 @@ public:
 
 		// Last resort: check the calling convention
 		return (f->getCallingConv() == CallingConv::PTX_Kernel || f->getCallingConv() == CallingConv::PTX_Device);
+  }*/
+
+  static void cacheAnnotationFromMD(const MDNode *md, key_val_pair_t &retval) {
+    MutexGuard Guard(Lock);
+    assert(md && "Invalid mdnode for annotation");
+    assert((md->getNumOperands() % 2) == 1 && "Invalid number of operands");
+    // start index = 1, to skip the global variable key
+    // increment = 2, to skip the value for each property-value pairs
+    for (unsigned i = 1, e = md->getNumOperands(); i != e; i += 2) {
+      // property
+      const MDString *prop = dyn_cast<MDString>(md->getOperand(i));
+      assert(prop && "Annotation property not a string");
+
+      // value
+      ConstantInt *Val = mdconst::dyn_extract<ConstantInt>(md->getOperand(i + 1));
+      assert(Val && "Value operand not a constant int");
+
+      std::string keyname = prop->getString().str();
+      if (retval.find(keyname) != retval.end())
+        retval[keyname].push_back(Val->getZExtValue());
+      else {
+        std::vector<unsigned> tmp;
+        tmp.push_back(Val->getZExtValue());
+        retval[keyname] = tmp;
+      }
+    }
+  }
+
+  static void cacheAnnotationFromMD(const Module *m, const GlobalValue *gv) {
+    MutexGuard Guard(Lock);
+    NamedMDNode *NMD = m->getNamedMetadata("nvvm.annotations");
+    if (!NMD)
+      return;
+    key_val_pair_t tmp;
+    for (unsigned i = 0, e = NMD->getNumOperands(); i != e; ++i) {
+      const MDNode *elem = NMD->getOperand(i);
+
+      GlobalValue *entity =
+          mdconst::dyn_extract_or_null<GlobalValue>(elem->getOperand(0));
+      // entity may be null due to DCE
+      if (!entity)
+        continue;
+      if (entity != gv)
+        continue;
+
+      // accumulate annotations for entity in tmp
+      cacheAnnotationFromMD(elem, tmp);
+    }
+
+    if (tmp.empty()) // no annotations for this gv
+      return;
+
+    if ((*annotationCache).find(m) != (*annotationCache).end())
+      (*annotationCache)[m][gv] = std::move(tmp);
+    else {
+      global_val_annot_t tmp1;
+      tmp1[gv] = std::move(tmp);
+      (*annotationCache)[m] = std::move(tmp1);
+    }
+  }
+
+  bool findOneNVVMAnnotation(const GlobalValue *gv, const std::string &prop,
+                             unsigned &retval) {
+    MutexGuard Guard(Lock);
+    const Module *m = gv->getParent();
+    if ((*annotationCache).find(m) == (*annotationCache).end())
+      cacheAnnotationFromMD(m, gv);
+    else if ((*annotationCache)[m].find(gv) == (*annotationCache)[m].end())
+      cacheAnnotationFromMD(m, gv);
+    if ((*annotationCache)[m][gv].find(prop) == (*annotationCache)[m][gv].end())
+      return false;
+    retval = (*annotationCache)[m][gv][prop][0];
+    return true;
+  }
+
+  bool isAKernelFunction(const Function &F) {
+    unsigned x = 0;
+    bool retval = findOneNVVMAnnotation(&F, "kernel", x);
+    if (!retval) {
+      // There is no NVVM metadata, check the calling convention
+      return F.getCallingConv() == CallingConv::PTX_Kernel;
+    }
+    return (x == 1);
   }
 
 	virtual bool runOnModule(Module &M)
@@ -87,7 +181,9 @@ public:
 				continue;
 
 			Function *F = &(*f);
-			if (isFunctionAKernel(F)) {
+			//if (isFunctionAKernel(F))
+			if (isAKernelFunction(*F))
+			{
 				if (FPInstrumentation::isUnwantedFunction(F))
 						continue;
 
