47e9ab14e996395c8439491f34a5ab72cf970067
Ignacio Laguna <ilaguna@llnl.gov>
2018-09-19 21:40:44 -0700

    added runtime upport for error messages (locked)

--- src/Runtime.h
+++ src/Runtime.h
@@ -41,31 +41,124 @@ __device__ void _FPC_FP64_CHECK_DIV_(float x, float y, float z, int loc);
 
 /* ----------------------------- Global Data ------------------------------- */
 
-#define TOOL_NAME "[FPChecker] "
+#define REPORT_LINE_SIZE 80
+#define REPORT_COL1_SIZE 15
+#define REPORT_COL2_SIZE REPORT_LINE_SIZE-REPORT_COL1_SIZE-1
 
 /// We store the file name and directory in this variable
 //char *_FPC_LOCATIONS_TABLE_[100];// = {"NONE1"};
 __device__ char *_FPC_FILE_NAME_[1];
 
 __device__ static int lock_state = 0;
-//__device__ static void lock(void) { while (atomicCAS(&lock_state, 0, 1) != 0); }
-__device__ static void unlock(void) { atomicExch(&lock_state, 0); }
 
 /* ------------------------ Generic Functions ------------------------------ */
 
+/// Report format
+/// +---------------- FPChecker Error Report ------------------+
+///  Error     : NaN
+///  Operation : DIV
+///  File      : /usr/file/pathForceCalculation.cc
+///  Line      : 23
+///  Thread ID : 1024
+/// +----------------------------------------------------------+
+
 __device__
-void _FPC_INTERRUPT_(int loc)
+static void _FPC_PRINT_REPORT_LINE_(const char border)
 {
-	//lock();
+	printf("%c",border);
+	for (int i=0; i < REPORT_LINE_SIZE-2; ++i)
+		printf("-");
+	printf("%c\n",border);
+}
+
+__device__
+static void _FPC_PRINT_REPORT_HEADER_(int type)
+{
+	//_FPC_PRINT_REPORT_LINE_('.');
+
+	char msg[255];
+	msg[0] = '\0';
+	if (type == 0)
+		strcpy(msg," FPChecker Error Report ");
+	else
+		strcpy(msg," FPChecker Warning Report ");
+
+	int l = strlen(msg);
+	l = REPORT_LINE_SIZE-l-2;
+	char line[255];
+	line[0] = '\0';
+	strcat(line,"+");
+	for (int i=0; i < l/2; ++i)
+			strcat(line,"-");
+	if (l%2)
+		strcat(line,"-");
+	strcat(line,msg);
+	for (int i=0; i < l/2; ++i)
+			strcat(line,"-");
+	strcat(line,"+");
+	printf("%s\n",line);
+}
+
+__device__
+static void _FPC_PRINT_REPORT_ROW_(const char *val, int space, int last)
+{
+	char msg[255];
+	msg[0] = '\0';
+	strcpy(msg," ");
+	strcat(msg, val);
+	int rem = strlen(msg);
+	for (int i=0; i < space-rem; ++i)
+		strcat(msg," ");
+	printf("%s",msg);
+
+	if (last==0)
+		printf(":");
+	else
+		printf("\n");
+}
+
+/// errorType: 0:NaN, 1:INF, 2:Underflow
+/// op: 0:ADD, 1:SUB, 2:MUL, 3:DIV
+__device__
+void _FPC_INTERRUPT_(int errorType, int op, int loc)
+{
 	bool blocked = true;
   	while(blocked) {
-    		if(0 == atomicCAS(&lock_state, 0, 1)) {
-		printf(TOOL_NAME "File: %s, Line: %d\n", _FPC_FILE_NAME_[0], loc);
-		//fflush(stdout);
-		asm("trap;");
+			if(0 == atomicCAS(&lock_state, 0, 1)) {
+
+				char e[64]; e[0] = '\0';
+				char o[64]; o[0] = '\0';
+				char l[64]; l[0] = '\0';
+
+				if 			(errorType == 0) strcpy(e, "NaN");
+				else if	(errorType == 1) strcpy(e, "INF");
+				else if	(errorType == 2) strcpy(e, "Underflow");
+				else strcpy(e, "NONE");
+
+				if 			(op == 0) strcpy(o, "ADD");
+				else if	(op == 1) strcpy(o, "SUB");
+				else if	(op == 2) strcpy(o, "MUL");
+				else if	(op == 3) strcpy(o, "DIV");
+				else strcpy(o, "NONE");
+
+				sprintf(l, "%d", loc);
+
+				_FPC_PRINT_REPORT_HEADER_(0);
+				_FPC_PRINT_REPORT_ROW_("Error", REPORT_COL1_SIZE, 0);
+				_FPC_PRINT_REPORT_ROW_(e, REPORT_COL2_SIZE, 1);
+				_FPC_PRINT_REPORT_ROW_("Operation", REPORT_COL1_SIZE, 0);
+				_FPC_PRINT_REPORT_ROW_(o, REPORT_COL2_SIZE, 1);
+				_FPC_PRINT_REPORT_ROW_("File", REPORT_COL1_SIZE, 0);
+				_FPC_PRINT_REPORT_ROW_(_FPC_FILE_NAME_[0], REPORT_COL2_SIZE, 1);
+				_FPC_PRINT_REPORT_ROW_("Line", REPORT_COL1_SIZE, 0);
+				_FPC_PRINT_REPORT_ROW_(l, REPORT_COL2_SIZE, 1);
+				_FPC_PRINT_REPORT_LINE_('+');
+
+				//printf(TOOL_NAME "File: %s, Line: %d\n", _FPC_FILE_NAME_[0], loc);
+
+				asm("trap;");
 		}
 	}
-	//unlock(); // never reached
 }
 
 /// Check the operation.
