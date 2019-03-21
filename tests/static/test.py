4fd00dc3b5fc26afd908ea0b4676615234990904
Ignacio Laguna <ilaguna@llnl.gov>
2019-03-21 13:49:04 -0700

    added make test

--- tests/static/test.py
+++ tests/static/test.py
@@ -13,7 +13,7 @@ def main():
     t = "Test: find instrumentation functions"
     testTarget = test_config.textWidth.format(t)
     sys.stdout.write(testTarget)
-    os.chdir("./test_find_inst_functions/")
+    os.chdir(test_config.path + "/test_find_inst_functions/")
     cmd = ["./test.py"]
     cmdOutput = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     sys.stdout.write(cmdOutput)
@@ -24,7 +24,7 @@ def main():
     t = "Test: num. fp operations"
     testTarget = test_config.textWidth.format(t)
     sys.stdout.write(testTarget)
-    os.chdir("./test_number_fp_operations/")
+    os.chdir(test_config.path + "/test_number_fp_operations/")
     cmd = ["./test.py"]
     cmdOutput = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     sys.stdout.write(cmdOutput)
@@ -35,7 +35,7 @@ def main():
     t = "Test: a device function is found"
     testTarget = test_config.textWidth.format(t)
     sys.stdout.write(testTarget)
-    os.chdir("./test_device_func_found/")
+    os.chdir(test_config.path + "/test_device_func_found/")
     cmd = ["./test.py"]
     cmdOutput = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     sys.stdout.write(cmdOutput)
@@ -46,7 +46,7 @@ def main():
     t = "Test: a global function is found"
     testTarget = test_config.textWidth.format(t)
     sys.stdout.write(testTarget)
-    os.chdir("./test_global_func_found/")
+    os.chdir(test_config.path + "/test_global_func_found/")
     cmd = ["./test.py"]
     cmdOutput = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     sys.stdout.write(cmdOutput)
@@ -57,7 +57,7 @@ def main():
     t = "Test: main() is found"
     testTarget = test_config.textWidth.format(t)
     sys.stdout.write(testTarget)
-    os.chdir("./test_main_is_found/")
+    os.chdir(test_config.path + "/test_main_is_found/")
     cmd = ["./test.py"]
     cmdOutput = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     sys.stdout.write(cmdOutput)
@@ -68,7 +68,7 @@ def main():
     t = "Test: global array instrumentation"
     testTarget = test_config.textWidth.format(t)
     sys.stdout.write(testTarget)
-    os.chdir("./test_global_array_instrumentation/")
+    os.chdir(test_config.path + "/test_global_array_instrumentation/")
     cmd = ["./test.py"]
     cmdOutput = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     sys.stdout.write(cmdOutput)
@@ -79,7 +79,7 @@ def main():
     t = "Test: correct func are found and instrumented"
     testTarget = test_config.textWidth.format(t)
     sys.stdout.write(testTarget)
-    os.chdir("./test_correct_inst_functions_found/")
+    os.chdir(test_config.path + "/test_correct_inst_functions_found/")
     cmd = ["./test.py"]
     cmdOutput = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     sys.stdout.write(cmdOutput)
