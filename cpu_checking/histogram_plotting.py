75e405037cfda7c0f407852a1b8d03ffcca94fcc
tanmaytirpankar <tirpankartanmay@gmail.com>
2021-10-15 17:53:56 -0600

    Added per file and program histogramming functions

--- cpu_checking/histogram_plotting.py
+++ cpu_checking/histogram_plotting.py
@@ -1,3 +1,4 @@
+import collections
 import glob
 import json
 import os
@@ -6,10 +7,127 @@ import matplotlib.pyplot as plt
 
 
 def load_report(file_name):
-    f = open(fileName, 'r')
+    f = open(file_name, 'r')
     data = json.load(f)
     f.close()
     return data
+
+
+# Accumulates quantity in data dictionary over specified key and adds to init_dict
+# init_dict and data need to have same structure
+def accumulate_over_key(init_dict, data, key):
+    for exponent in data[key].keys():
+        if exponent in init_dict[key]:
+            init_dict[key][exponent] += data[key][exponent]
+        else:
+            init_dict[key][exponent] = data[key][exponent]
+
+    return init_dict
+
+
+# Generates exponent plots for each source code line recorded in the histogram_data in the
+# directory plots_root_path
+def histogram_per_program(plots_root_path, histogram_data):
+    # Create a new directory if does not exist
+    if not os.path.isdir(plots_root_path):
+        os.mkdir(plots_root_path)
+
+    accumulated_exponent_dict = {'fp32': {}, 'fp64': {}}
+    plot_name = os.path.basename(histogram_data[0]['input'])
+
+    # Looping through each line recorded in histogram json file and accumulating exponent data
+    for line_data in histogram_data:
+        accumulated_exponent_dict = accumulate_over_key(accumulated_exponent_dict, line_data, 'fp32')
+        accumulated_exponent_dict = accumulate_over_key(accumulated_exponent_dict, line_data, 'fp64')
+
+    # Filling missing exponent records with 0s in fp32 and fp64 dictionaries for plotting purposes
+    keys_set = set(accumulated_exponent_dict['fp32'].keys())
+    keys_set.update(accumulated_exponent_dict['fp64'].keys())
+    fp32_dictionary = dict.fromkeys(keys_set, 0)
+    fp64_dictionary = dict.fromkeys(keys_set, 0)
+    fp32_dictionary.update(accumulated_exponent_dict['fp32'])
+    fp64_dictionary.update(accumulated_exponent_dict['fp64'])
+
+    # Data gathered for plotting
+    x_axis_values = list(fp32_dictionary.keys())
+    x_axis_label_position = list(range(len(x_axis_values)))
+    y_axis_fp32_values = list(fp32_dictionary.values())
+    y_axis_fp64_values = list(fp64_dictionary.values())
+
+    # Multibar plotting begins
+    plt.clf()
+    plt.xticks(x_axis_label_position, x_axis_values)
+
+    x_axis_label_position[:] = [number - 0.2 for number in x_axis_label_position]
+    plt.bar(x_axis_label_position, y_axis_fp32_values, 0.4, label="fp32")
+
+    x_axis_label_position[:] = [number + 0.4 for number in x_axis_label_position]
+    plt.bar(x_axis_label_position, y_axis_fp64_values, 0.4, label="fp64")
+
+    plt.legend()
+    plt.xlabel('Exponent')
+    plt.ylabel('Counts')
+
+    # Saving figure as the source file name
+    plt.savefig(plots_root_path + plot_name + '.png')
+
+
+# Generates exponent plots for each source code line recorded in the histogram_data in the
+# directory plots_root_path
+def histogram_per_file(plots_root_path, histogram_data):
+    # Create a new directory if does not exist
+    if not os.path.isdir(plots_root_path):
+        os.mkdir(plots_root_path)
+
+    accumulated_exponent_dict = {}
+    # Looping through each line recorded in histogram json file and accumulating exponent data per file
+    for line_data in histogram_data:
+        # File name in histogram json is the complete path. Extract the base name.
+        file_name = os.path.basename(line_data['file'])
+
+        if file_name not in accumulated_exponent_dict:
+            accumulated_exponent_dict[file_name] = {'fp32': {}, 'fp64': {}}
+
+        accumulated_exponent_dict[file_name] = accumulate_over_key(accumulated_exponent_dict[file_name], line_data,
+                                                                   'fp32')
+        accumulated_exponent_dict[file_name] = accumulate_over_key(accumulated_exponent_dict[file_name], line_data,
+                                                                   'fp64')
+
+    # Looping through each record which corresponds to a file
+    for file_name, file_data in accumulated_exponent_dict.items():
+        split_file_name = os.path.splitext(file_name)
+        plot_name = split_file_name[0] + split_file_name[1].split('.')[1].capitalize()
+
+        # Filling missing exponent records with 0s in fp32 and fp64 dictionaries for plotting purposes
+        keys_set = set(file_data['fp32'].keys())
+        keys_set.update(file_data['fp64'].keys())
+        fp32_dictionary = dict.fromkeys(keys_set, 0)
+        fp64_dictionary = dict.fromkeys(keys_set, 0)
+        fp32_dictionary.update(file_data['fp32'])
+        fp64_dictionary.update(file_data['fp64'])
+
+        # Data gathered for plotting
+        x_axis_values = list(fp32_dictionary.keys())
+        x_axis_label_position = list(range(len(x_axis_values)))
+        y_axis_fp32_values = list(fp32_dictionary.values())
+        y_axis_fp64_values = list(fp64_dictionary.values())
+
+        # Multibar plotting begins
+        plt.clf()
+        plt.xticks(x_axis_label_position, x_axis_values)
+
+        x_axis_label_position[:] = [number - 0.2 for number in x_axis_label_position]
+        plt.bar(x_axis_label_position, y_axis_fp32_values, 0.4, label="fp32")
+
+        x_axis_label_position[:] = [number + 0.4 for number in x_axis_label_position]
+        plt.bar(x_axis_label_position, y_axis_fp64_values, 0.4, label="fp64")
+
+        plt.legend()
+        plt.xlabel('Exponent')
+        plt.ylabel('Counts')
+
+        # Saving figure as the source file name
+        plt.savefig(plots_root_path + plot_name + '.png')
 
 
 # Generates exponent plots for each source code line recorded in the histogram_data in the
@@ -31,32 +149,29 @@ def histogram_per_line(plots_root_path, histogram_data):
             os.mkdir(plots_root_path + plots_for_file_directory)
         plots_for_file_directory += '/'
 
-        # Filling missing exponent records with 0s in fp32 and fp64 dictionaries for plotting purposes.
-        new_keys_set = set(line_data['fp32'].keys())
-        new_keys_set.update(line_data['fp64'].keys())
-        fp32_dictionary = dict.fromkeys(new_keys_set, 0)
-        fp64_dictionary = dict.fromkeys(new_keys_set, 0)
+        # Filling missing exponent records with 0s in fp32 and fp64 dictionaries for plotting purposes
+        keys_set = set(line_data['fp32'].keys())
+        keys_set.update(line_data['fp64'].keys())
+        fp32_dictionary = dict.fromkeys(keys_set, 0)
+        fp64_dictionary = dict.fromkeys(keys_set, 0)
         fp32_dictionary.update(line_data['fp32'])
         fp64_dictionary.update(line_data['fp64'])
 
         # Data gathered for plotting
         x_axis_values = list(fp32_dictionary.keys())
-        x_axis_label_pos = list(range(len(x_axis_values)))
+        x_axis_label_position = list(range(len(x_axis_values)))
         y_axis_fp32_values = list(fp32_dictionary.values())
         y_axis_fp64_values = list(fp64_dictionary.values())
 
-        # print(x_axis_values)
-        # print(y_axis_values)
-
         # Multibar plotting begins
         plt.clf()
-        plt.xticks(x_axis_label_pos, x_axis_values)
+        plt.xticks(x_axis_label_position, x_axis_values)
 
-        x_axis_label_pos[:] = [number - 0.2 for number in x_axis_label_pos]
-        plt.bar(x_axis_label_pos, y_axis_fp32_values, 0.4, label="fp32")
+        x_axis_label_position[:] = [number - 0.2 for number in x_axis_label_position]
+        plt.bar(x_axis_label_position, y_axis_fp32_values, 0.4, label="fp32")
 
-        x_axis_label_pos[:] = [number + 0.4 for number in x_axis_label_pos]
-        plt.bar(x_axis_label_pos, y_axis_fp64_values, 0.4, label="fp64")
+        x_axis_label_position[:] = [number + 0.4 for number in x_axis_label_position]
+        plt.bar(x_axis_label_position, y_axis_fp64_values, 0.4, label="fp64")
 
         plt.legend()
         plt.xlabel('Exponent')
@@ -72,6 +187,8 @@ if __name__ == '__main__':
     histogramData = load_report(fileName)
 
     # json_formatted_obj = json.dumps(histogramData, indent=2)
-    # print(histogramData)
+    # print(json_formatted_obj)
 
-    histogram_per_line(plotsRootPath, histogramData)
+    # histogram_per_line(plotsRootPath, histogramData)
+    # histogram_per_file(plotsRootPath, histogramData)
+    histogram_per_program(plotsRootPath, histogramData)
